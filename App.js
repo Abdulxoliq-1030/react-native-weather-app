b2d23d26832014d806362e71ac09dee3532ed323
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-14 10:50:38 +0500

    react native expo components

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.text}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "orange",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  text: {
+    width: "50%",
+    backgroundColor: "blue",
+    fontSize: 24,
+    color: "#fff",
   },
 });
